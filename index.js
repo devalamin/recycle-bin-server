27ea05c172e6b50bf3fe84933cf7332f028dda4f
devalamin <alamin.hasibmd2017@gmail.com>
2022-11-24 01:57:22 +0600

    categories api created

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+require('dotenv').config()
+
 const app = express();
 
-
+// middlewares
 app.use(cors());
+app.use(express.json());
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('hello world')
 });
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_RECYCLE}:${process.env.DB_PASSWORD_RECYCLE}@cluster0.3b5klku.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('recyclebindb').collection('categories');
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const categories = await categoriesCollection.find(query).toArray()
+            res.send(categories)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.error(error))
+
+
+
+app.get('/categories/:products', async (req, res) => {
+
+})
+
 app.listen(port, () => {
     console.log('server is running on', port);
-})
+});
